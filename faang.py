f957bb745e47b2c5954bbf4165390945afafc8b1
LoicBagnoud <Sirktar@gmail.com>
2025-11-26 15:47:01 +0000

    Added Actions

--- faang.py
+++ faang.py
@@ -23,4 +23,27 @@ time_format = now.strftime("%Y%m%d-%H%M%S")
 data = f"data/{time_format}.csv"
 df.to_csv(data)
 
-# MUST INSERT A PLOT HERE
+# Create new figure and axis.
+fig, ax = plt.subplots()
+
+# Plot all closing prices.
+df['Close'].plot(ax=ax)
+
+# Give our labels and titles. 
+
+# We need to force the legend to go to the upper right, since it was in the middle of the plot.
+ax.legend(loc='upper right')
+
+ax.set_title("Close Price – Last 5 Days")
+ax.set_xlabel("Date")
+ax.set_ylabel("Price")
+
+# # Format timestamp for filename.
+now = datetime.now()
+time_format = now.strftime("%Y%m%d-%H%M%S")
+
+# Build file path inside existing 'plots' folder.
+filepath = f"plots/{time_format}.png"
+
+# # Save it with our desired resolution.
+fig.savefig(filepath, dpi=300)
